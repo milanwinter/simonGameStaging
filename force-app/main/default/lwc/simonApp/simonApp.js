2e9773c01b86078ccc7bcd74ec2018389114816c
milanwinter <63170789+milanwinter@users.noreply.github.com>
2021-09-15 09:27:25 -0700

    added svg and some css, started building out js file

---
+++ force-app/main/default/lwc/simonApp/simonApp.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class SimonApp extends LightningElement {
+
+    simon = true;
+    currentPath;
+
+    pathGenerator() {
+        let choices = [0,1,2,3]
+    }
+
+    clickSection() {
+
+    }
+}
